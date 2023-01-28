4575f91bdaaacf6329acb04d4f76afbe909ccf73
Georgiy_Gritsenko <gritsenko.1932@yandex.ru>
2023-01-28 23:59:23 +0300

    second method

--- finalWORK/Program.cs
+++ finalWORK/Program.cs
@@ -3,7 +3,7 @@ using static System.Console;
 Clear();
 
 string[] array = EnteredArray();
-
+string[] result = FindLessFour(array, 4);
 
 
 
@@ -11,4 +11,15 @@ string[] EnteredArray() {
     Write("Введите строки через пробел: ");
     return ReadLine().Split(" ");
 }
+string[] FindLessFour(string[] input, int n) {
+    string[] output = new string[CountLessFour(input, n)];
 
+    for(int i = 0, a = 0; i < input.Length; i++) {
+        if(input[i].Length = n) {
+            output[a] = input[i];
+            a++;
+        }
+    }
+
+    return output;
+}
