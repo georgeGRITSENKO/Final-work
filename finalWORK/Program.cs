54b264e7c6a99bd3a550fd6a31012f64b7f2373a
Georgiy_Gritsenko <gritsenko.1932@yandex.ru>
2023-01-29 00:06:45 +0300

    completed code

--- finalWORK/Program.cs
+++ finalWORK/Program.cs
@@ -5,7 +5,7 @@ Clear();
 string[] array = EnteredArray();
 string[] result = FindLessFour(array, 4);
 
-
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
 
 string[] EnteredArray() {
     Write("Введите строки через пробел: ");
@@ -15,11 +15,22 @@ string[] FindLessFour(string[] input, int n) {
     string[] output = new string[CountLessFour(input, n)];
 
     for(int i = 0, a = 0; i < input.Length; i++) {
-        if(input[i].Length = n) {
+        if(input[i].Length < n) {
             output[a] = input[i];
             a++;
         }
     }
 
     return output;
+}
+int CountLessFour(string[] input, int n) {
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length < n) {
+            count++;
+        }
+    }
+
+    return count;
 }
